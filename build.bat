25511d8109af2e7f55d4ab5f18d83352f11b78dc
yoz75 <levtannn@gmail.com>
2025-08-06 12:39:13 +0300

    fixed building

--- build.bat
+++ build.bat
@@ -1,1 +1,5 @@
+@echo off
 dub build --compiler=ldc2 --build=debug
+
+copy raylib.dll raylibtemp.dll
+move raylibtemp.dll ./bin/raylib.dll
--- buildr.bat
+++ buildr.bat
@@ -1,1 +1,4 @@
 dub build --compiler=ldc2 --build=release
+
+copy raylib.dll raylibtemp.dll
+move raylibtemp.dll ./bin/raylib.dll
