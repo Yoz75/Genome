8054d77808ffdd16fb49c91f4dd8666115657745
yoz75 <levtannn@gmail.com>
2025-08-06 13:21:08 +0300

    updated default config values

--- source/genome/settings/config.d
+++ source/genome/settings/config.d
@@ -41,14 +41,14 @@ public:
 
     @AskUser("cost of walking"d) float walkEnergyCost = 1;
 
-    @AskUser("energy damage of starving (every tick)"d) float starveEnergy = .5;
-    @AskUser("energy gain from eating other agents"d) float cannibalismEnergy = 10;
+    @AskUser("energy damage of starving (every tick)"d) float starveEnergy = .1;
+    @AskUser("energy gain from eating other agents"d) float cannibalismEnergy = 2.5;
     @AskUser("energy gain from eating food"d) float foodEnergy = 10;
-    @AskUser("damage gain from eating a spike"d) float spikeEnergyDamage = 10;
+    @AskUser("damage gain from eating a spike"d) float spikeEnergyDamage = 999;
 
     @AskUser("how many instructions in agent's genome?"d) int genomeSize = 64;
     @AskUser("maximal instruction's value. Use -1 to use only instructions"d) int maxInstructionValue = 64;
-    @AskUser("maximal executed commands per frame"d) int maxExecutedCommands = 4;
+    @AskUser("maximal executed commands per frame"d) int maxExecutedCommands = 2;
     @AskUser("minimal count of genome mutations"d) int minMutationsCount = 0;
     @AskUser("maximal count of genome mutations"d) int maxMutationsCount = 5;
 }
